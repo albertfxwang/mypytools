b94730f005006afe141cf1359594f26fa9c71742
Kuang-Han Huang <astrokuang@gmail.com>
2014-10-20 11:37:35 -0700

    delete all *.pyc files; update galev.py to read model SED physical properties.

--- sedtools/galev.py
+++ sedtools/galev.py
@@ -10,18 +10,24 @@ from scipy.interpolate import interp1d
 pc_cm = 3.0857e18  # 1 parsec in cm
 area_10pc = 4. * np.pi * (10 * pc_cm)**2
 
-def writeTemplates(libraryFile, root=None, overwrite=True, ageUniverse=13.8, tempdir='/Users/khuang/Dropbox/codes/phot_z/eazy/GALEV'):
+def writeTemplates(libraryFile, statFile, root=None, overwrite=True, ageUniverse=13.8, tempdir='/Users/khuang/Dropbox/codes/phot_z/eazy/GALEV'):
    """
    From a GALEV output template library *_spec.dat, write individual SED 
    template files. All files share a common root, which by default is what
    goes before the _spec.data in the library file name.
+   Also read the physical property file and add stellar masses, gas masses, 
+   SFR, and metallicity (Z) to each file.
    """
    libraryFile = os.path.split(libraryFile)[-1]
+   statFile = os.path.split(statFile)[-1]
    if root == None:
       root = libraryFile.split('_spec.dat')[0]
    f = open(libraryFile, 'rb')
    lines = f.readlines()
    f.close()
+   f2 = open(statFile, 'rb')
+   lines2 = f2.readlines()
+   f2.close()
    # First, read the number of age and wavelength steps
    l0 = lines[0].split()[1:]
    numAges = int(l0[0])   # number of age steps
@@ -38,6 +44,22 @@ def writeTemplates(libraryFile, root=None, overwrite=True, ageUniverse=13.8, tem
       # print "len(l):", len(l)
       wavetable[i] = float(l[1])
       fluxtable[i] = [float(y) for y in l[2:]]
+   # Also read the physical properties
+   GMass = np.zeros(numAges)  # Gas mass
+   SMass = np.zeros(numAges)  # Stellar mass
+   SFR = np.zeros(numAges)    # SFR
+   logZ = np.zeros(numAges)   # log(Z)
+   m = 0  # counter for GMass, SMass, etc.
+   for l2 in lines2:
+      if l2.startswith('#'):
+         pass
+      else:
+         list2 = l2.split()  # split the columns in statFile
+         GMass[m] = float(list2[1])  
+         SMass[m] = float(list2[2])
+         SFR[m] = float(list2[3])
+         logZ[m] = float(list2[4])
+         m += 1
    # Now write to individual files
    if not os.path.isdir(tempdir):
       os.mkdir(tempdir)
@@ -65,6 +87,10 @@ def writeTemplates(libraryFile, root=None, overwrite=True, ageUniverse=13.8, tem
          fj = open('%s/%s_age%.4f.dat' % (tempdir, root, logAge), 'wb')
          fj.write('# Root = %s\n' % root)
          fj.write('# Age = %.5e\n' % agetable[j])
+         fj.write('# GMass = %.5e\n' % GMass[j])
+         fj.write('# SMass = %.5e\n' % SMass[j])
+         fj.write('# SFR = %.5e\n' % SFR[j])
+         fj.write('# logZ = %.5e\n' % logZ[j])
          fj.write('# angstrom   f_lam\n')
          for i in range(numWaves):
             fj.write('%6.2f %.6e\n' % (wavetable[i], fluxtable[i][j]))
