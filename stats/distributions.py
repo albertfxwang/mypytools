cc2536efc366c8ea2e20a825824c967cbb143644
Kuang-Han Huang <astrokuang@gmail.com>
2014-10-28 14:26:19 -0700

    Add distributions.py to estimate PDF, CDF, and confidence intervals of a distribution given 1-D data points.

---
+++ stats/distributions.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+import numpy as np
+import scipy
+from scipy import stats
+
+class Distribution1D(object):
+   """
+   Given a 1-D list (array) of values, estimate the PDF with Gaussian KDE and 
+   CDF at a grid of points specified by the user.
+   """
+   def __init__(self, values, bw_method='silverman'):
+      """
+      Values: a list (array) of data points based on which we estimate the PDF.
+      """
+      self.values = np.array(values).astype('float')
+      self.kernel = stats.gaussian_kde(self.values, bw_method=bw_method)
+
+   def PDF(self, xgrid):
+      """
+      Use Gaussian KDE to estimate the PDF.
+      xgrid: the grid of x values where we estimate the PDF.
+      """
+      return [xgrid, self.kernel.evaluate(xgrid)]
+
+   def CDF(self, xgrid):
+      """
+      Use Gaussian KDE to estimate the CDF.
+      xgrid: the grid of x values where we estimate the CDF.
+      """
+      pdf = self.kernel.evaluate(xgrid)
+      cdf = np.cumsum(pdf)
+      return [xgrid, cdf]
+
+   def conf_interval(self, xgrid, p=0.68, x0=None):
+      """
+      Estimate the confidence interval for the estimated PDF, given the xgrid
+      that the user provides to estimate the CDF.
+      p is the total probability enclosed by the interval.
+      Optionally, the user can provide the best value x0 around which to 
+      construct the confidence interval; the code doesn't really use x0 to 
+      compute the confidence interval, but checks if x0 lies within the 
+      estimated interval. If not, it will print a warning message and does 
+      nothing.
+      """
+      ptail = (1. - p) / 2.  # the probilities in both tails
+      pLow = ptail
+      pHigh = 1. - ptail
+      print pLow, pHigh
+      cdf = self.CDF(xgrid)[1]
+      cdf = cdf / cdf.max()  # normalize max(CDF) to 1
+      xinterval = np.sort(xgrid[(cdf >= pLow) & (cdf <= pHigh)])
+      if x0 != None:
+         if (x0 < xinterval[0]) or (x0 > xinterval[-1]):
+            print "Warning: x0 is NOT within the interval [%.3f, %.3f]!!" % (xinterval[0], xinterval[-1])
+      return xinterval[0], xinterval[-1]
