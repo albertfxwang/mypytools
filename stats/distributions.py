d4cdc2c0e288ad664e3c836652d377dcb9ab9397
Kuang-Han Huang <astrokuang@gmail.com>
2015-01-12 17:08:55 -0800

    Switch to using scipy.stats.rv_discrete for discrete CDF/PDF.

--- stats/distributions.py
+++ stats/distributions.py
@@ -4,17 +4,22 @@ import numpy as np
 import scipy
 from scipy import stats
 
-class Distribution1D(object):
+class Distribution1D(stats.rv_discrete):
    """
    Given a 1-D list (array) of values, estimate the PDF with Gaussian KDE and 
    CDF at a grid of points specified by the user.
    """
-   def __init__(self, values, bw_method='silverman'):
+   def __init__(self, values, weights=-1, bw_method='silverman'):
       """
       Values: a list (array) of data points based on which we estimate the PDF.
       """
-      self.values = np.array(values).astype('float')
-      self.kernel = stats.gaussian_kde(self.values, bw_method=bw_method)
+      # self.values = np.array(values).astype('float')
+      if weights < 0:
+         weights = np.ones(len(values)) / len(values) # equal weights
+      assert len(weights) == len(values)
+      weights = np.array(weights) / np.sum(weights)
+      stats.rv_discrete.__init__(self, a=0, b=1, values=(values, weights))
+      self.kernel = stats.gaussian_kde(values, bw_method=bw_method)
 
    def PDF(self, xgrid):
       """
@@ -28,17 +33,15 @@ class Distribution1D(object):
       Use Gaussian KDE to estimate the CDF.
       xgrid: the grid of x values where we estimate the CDF.
       """
-      pdf = self.kernel.evaluate(xgrid)
-      cdf = np.cumsum(pdf)
-      return [xgrid, cdf]
+      return self.cdf(xgrid)
 
    def minmax(self):
       """
       Return the (min, max) of self.values.
       """
-      return self.values.min(), self.values.max()
+      return self.xk[0], self.xk[-1]
 
-   def conf_interval(self, xgrid=100, p=0.68, x0=None, print_it=False):
+   def conf_interval(self, xgrid=None, p=0.68, x0=None, print_it=False, scale=1):
       """
       Estimate the confidence interval for the estimated PDF, given the xgrid
       that the user provides to estimate the CDF.
@@ -54,14 +57,31 @@ class Distribution1D(object):
       ptail = (1. - p) / 2.  # the probilities in both tails
       pLow = ptail
       pHigh = 1. - ptail
-      if type(xgrid) == type(10):
+      if xgrid == None:
+         xgrid = self.xk
+      elif type(xgrid) == type(10):
          vmin, vmax = self.minmax()
          xgrid = np.linspace(vmin, vmax, xgrid+1)
-      cdf = self.CDF(xgrid)[1]
-      cdf = cdf / cdf.max()  # normalize max(CDF) to 1
+      cdf = self.CDF(xgrid)
+      # cdf = cdf / cdf.max()  # normalize max(CDF) to 1
       xinterval = np.sort(xgrid[(cdf >= pLow) & (cdf <= pHigh)])
+      xinterval = scale * xinterval
+      # print len(xinterval)
       if x0 != None:
+         x0 = scale * x0
          if (x0 < xinterval[0]) or (x0 > xinterval[-1]):
-            print "Warning: x0 (%.3f) is NOT within the interval [%.3f, %.3f]!!" % (x0, xinterval[0], xinterval[-1])
+            print "Warning: x0 (%.3f) is NOT within the default interval [%.3f, %.3f]!!" % (x0, xinterval[0], xinterval[-1])
+            if (x0 < xinterval[0]):
+               print "Set CDF(x0) to pLow..."
+               xlow = np.max(xgrid[xgrid<=(x0/scale)])
+               pLow = self.CDF(xlow)
+               pHigh = pLow + p
+            else:
+               print "Set CDF(x0) to pHigh..."
+               xhigh = np.min(xgrid[xgrid>=(x0/scale)])
+               pHigh = self.CDF(xhigh)
+               pLow = pHigh - p
+            print "New values for pLow, pHigh:", pLow, pHigh
+            xinterval = np.sort(xgrid[(cdf >= pLow) & (cdf <= pHigh)]) * scale
          if print_it: print "Confidence interval: %.4f (+ %.4f) (- %.4f)" % (x0, (xinterval[-1]-x0), (x0-xinterval[0]))
       return xinterval[0], xinterval[-1]
